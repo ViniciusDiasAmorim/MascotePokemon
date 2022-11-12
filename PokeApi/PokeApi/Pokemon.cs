e6e6f66e132e0087f6753a361d8c94aee0b2c145
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-11-12 10:18:35 -0300

    day 1 requisitando dados da api

---
+++ PokeApi/PokeApi/Pokemon.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace PokeApi
+{
+    public partial class Pokemon
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("url")]
+        public Uri Url { get; set; }
+    }
+}
--- PokeApi/PokeApi/Program.cs
+++ PokeApi/PokeApi/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using PokeApi;
+
+HttpClient client = new HttpClient();
+
+string url = "https://pokeapi.co/api/v2/pokemon/";
+
+var response = await client.GetAsync(url);
+
+var content = await response.Content.ReadAsStringAsync();
+
+dynamic responseContent = JsonConvert.DeserializeObject(content);
+
+List<Pokemon> pokemons = new List<Pokemon>();
+
+foreach(var item in responseContent.results)
+{
+    Pokemon result = new Pokemon()
+    {
+        Name = item.name,
+        Url = item.url
+    };
+
+    pokemons.Add(result);
+}
+
+foreach(var item in pokemons)
+{
+    Console.WriteLine(item.Name);
+}
