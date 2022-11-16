1f50f74c7218dc6c3e1ba94997064ac968be8cd9
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-11-15 22:28:22 -0300

    day 04

--- PokeApi/PokeApi/Program.cs
+++ PokeApi/PokeApi/Program.cs
@@ -1,12 +1,9 @@
-﻿using Newtonsoft.Json;
-using PokeApi.Model;
-using PokeApi.Models;
+﻿using PokeApi.Models;
 using PokeApi.Services;
+using PokeApi.View;
 
 Console.WriteLine("Bem vindo ao PokeAdotaApi !!");
-
 Console.WriteLine("Qual o seu Nome ?");
-
 string nome = Console.ReadLine();
 
 Console.Clear();
@@ -15,15 +12,13 @@ bool rodaPrograma = true;
 
 while (rodaPrograma)
 {
-    Console.WriteLine("--------------------Menu--------------------");
-    Console.WriteLine($"{nome} oque gostaria de Fazer?");
-    Console.WriteLine("1 - Ver os Pokemons.");
-    Console.WriteLine("2 - Ver os Pokemons que voce capturou.");
-    Console.WriteLine("3 - Sair.");
+    PokemonService pokemonService = new PokemonService();
+
+    PokemonView pokemonView = new PokemonView();
+
+    pokemonView.MenuView(nome);
 
     string opt = Console.ReadLine();
-
-    PokemonService pokemonService = new PokemonService();
 
     switch (opt)
     {
@@ -37,24 +32,7 @@ while (rodaPrograma)
                 {
                     var pokemonsEncontrados = await pokemonService.GetAllPokemon(search.Results);
 
-                    foreach (var item in pokemonsEncontrados)
-                    {
-                        Console.WriteLine($"{item.Key} - {item.Value.Name}");
-                    }
-
-                    Console.WriteLine("Encontrou algum Pokemon que voce deseja?");
-
-                    if (search.Next != null)
-                    {
-                        Console.WriteLine("1 - Caso deseje avançar para a proxima pagina.");
-                    }
-
-                    if (search.Previous != null)
-                    {
-                        Console.WriteLine("2 - Caso deseje voltar para a pagina anterior.");
-                    }
-
-                    Console.WriteLine("3 - Para escolher um Pokemon desta lista");
+                    pokemonView.MostraPokemonsView(pokemonsEncontrados, search);
 
                     string optBusca = Console.ReadLine();
                     Console.Clear();
@@ -62,8 +40,27 @@ while (rodaPrograma)
                     switch (optBusca)
                     {
                         case "1": search = await pokemonService.SearchPokemonAsync(search.Next); break;
+
                         case "2": search = await pokemonService.SearchPokemonAsync(search.Previous); break;
-                        case "3": buscaMascote = false; var pokemonEscolhido = EscolhePokemon(pokemonsEncontrados, pokemonService); break;
+
+                        case "3": buscaMascote = false;
+                            Pokemon pokemonInfo = new Pokemon();
+
+                            Species pokemon = pokemonView.EscolhePokemonView(pokemonsEncontrados);
+
+                            string optPokemon = Console.ReadLine();
+
+                            switch (optPokemon)
+                            {
+                                case "1": pokemonInfo = await pokemonService.GetPokemon(pokemon.Url); break;
+                            }
+
+                            pokemonView.InformacaoDoPokemon(pokemonInfo);
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+
                         default:
                             Console.WriteLine("Acho que voce digitou um valor nao esperado. Voce sera redirecionado para o menu.");
                             buscaMascote = false;
@@ -73,45 +70,6 @@ while (rodaPrograma)
             }
             break;
 
-        case "3": rodaPrograma = false; break;
+        case "3": rodaPrograma = false; Console.WriteLine("Obrigado por utilizar o programa."); break;
     }
-}
-
-static async Task<Species> EscolhePokemon(Dictionary<int, Species> pokemonsEncontrados, PokemonService pokemonService)
-{
-    Pokemon pokemonInfo = new Pokemon();
-
-    foreach (var item in pokemonsEncontrados)
-    {
-        Console.WriteLine($"{item.Key} - {item.Value.Name}");
-    }
-
-    Console.WriteLine("Digite o numero do pokemon que voce deseja");
-
-    int pokemonEscolhido = int.Parse(Console.ReadLine());
-
-    if (pokemonsEncontrados.TryGetValue(pokemonEscolhido, out var pokemon)) ;
-    {
-        Console.WriteLine(pokemon.Name);
-    }
-
-    Console.WriteLine("Oque deseja?");
-    Console.WriteLine($"1 - Saber mais sobre o {pokemon.Name}");
-    Console.WriteLine($"2 - Adotar o {pokemon.Name}");
-    string opt = Console.ReadLine();
-    switch (opt)
-    {
-        case "1": pokemonInfo = await pokemonService.GetPokemon(pokemon.Url); break;
-    }
-    Console.Clear();
-    Console.WriteLine($"Nome: {pokemonInfo.Name}");
-    Console.WriteLine($"Altura: {pokemonInfo.Height}");
-    Console.WriteLine($"Peso: {pokemonInfo.Weight}");
-    Console.WriteLine($"Habilidades:");
-    foreach (var p in pokemonInfo.Abilities)
-    {
-        Console.WriteLine(p.AbilityAbility.Name);
-    }
-    Console.ReadLine();
-    return pokemon;
 }
---
+++ PokeApi/PokeApi/View/PokemonView.cs
@@ -0,0 +1,85 @@
+﻿using PokeApi.Model;
+using PokeApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi.View
+{
+    public class PokemonView
+    {
+        public void MenuView(string nome)
+        {
+            Console.WriteLine("--------------------Menu--------------------");
+            Console.WriteLine($"{nome} oque gostaria de Fazer?");
+            Console.WriteLine("1 - Ver os Pokemons.");
+            Console.WriteLine("2 - Ver os Pokemons que voce capturou.");
+            Console.WriteLine("3 - Sair.");
+        }
+
+        public void MostraPokemonsView(Dictionary<int, Species> pokemonsEncontrados, Search search)
+        {
+            Console.WriteLine("--------------------Pokemons Encontrados--------------------");
+
+            foreach (var item in pokemonsEncontrados)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value.Name}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Encontrou algum Pokemon que voce deseja?");
+            Console.WriteLine();
+            if (search.Next != null)
+            {
+                Console.WriteLine("1 - Caso deseje avançar para a proxima pagina.");
+            }
+            Console.WriteLine();
+            if (search.Previous != null)
+            {
+                Console.WriteLine("2 - Caso deseje voltar para a pagina anterior.");
+            }
+            Console.WriteLine();
+            Console.WriteLine("3 - Para escolher um Pokemon desta lista");
+        }
+
+        public Species EscolhePokemonView(Dictionary<int, Species> pokemonsEncontrados)
+        {
+            foreach (var item in pokemonsEncontrados)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value.Name}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero do pokemon que voce deseja");
+
+            int pokemonEscolhido = int.Parse(Console.ReadLine());
+
+            if (pokemonsEncontrados.TryGetValue(pokemonEscolhido, out var pokemon))
+            {
+                Console.WriteLine(pokemon.Name);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Oque deseja?");
+            Console.WriteLine();
+            Console.WriteLine($"1 - Saber mais sobre o {pokemon.Name}");
+            Console.WriteLine();
+            Console.WriteLine($"2 - Adotar o {pokemon.Name}");
+            Console.WriteLine();
+            
+            return pokemon;
+        }
+        public void InformacaoDoPokemon(Pokemon pokemon)
+        {
+            Console.Clear();
+            Console.WriteLine($"Nome: {pokemon.Name}");
+            Console.WriteLine($"Altura: {pokemon.Height}");
+            Console.WriteLine($"Peso: {pokemon.Weight}");
+            Console.WriteLine($"Habilidades:");
+            foreach (var p in pokemon.Abilities)
+            {
+                Console.WriteLine(p.AbilityAbility.Name);
+            }
+        }
+    }
+}
